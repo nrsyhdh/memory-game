9843ef73a3749a8561c7ae88cf2168983880a494
nrsyhdh <76397297+nrsyhdh@users.noreply.github.com>
2020-12-20 22:28:40 +0800

    Updated app.js

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ let match = 0
 let star3 = 8
 let star2 = 12
 let star1 = 16
+let star0 = 20
 
 const refreshPage = () => {
     setTimeout(()=>{
@@ -92,10 +93,12 @@ function resetBoard(){
 function setRating(move){
     var rating = 3
     if(move > star3 && move < star2){
-        rating = 2
+        rating = 3
     } else if(move > star2 && move < star1){
+        rating = 2
+    } else if(move > star1 && move < star0){
         rating = 1
-    } else if(move > star1){
+    } else if(move >= star0){
         rating = 0
     }
 
